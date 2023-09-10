3f22e132e8c102fb6687ec0b6455282166a4e31d
anna <lyne.nyangon@strathmore.edu>
2023-09-10 08:14:52 +0300

    :bulb: refactor: add comments

--- app/src/main/java/com/anna/healthyeats/ui/components/forms/common/field_key_options.kt
+++ app/src/main/java/com/anna/healthyeats/ui/components/forms/common/field_key_options.kt
@@ -4,12 +4,20 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 
+/**
+ * Represents options for keyboard inputs
+ */
 enum class KeyOptions{
   TEXT_INPUT,
   EMAIL_INPUT,
   PASSWORD_INPUT
 }
 
+/**
+ * Determines keyboard type to be returned based on KeyOptions instance provided
+ * @param keyOptions KeyOptions instance
+ * @return Keyboard type
+ */
 fun getKeyboard(keyOptions: KeyOptions): KeyboardOptions {
   return KeyboardOptions(
     keyboardType = when(keyOptions){
