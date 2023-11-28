8824e01493badee058c4e54163b27e165cba59b4
Lynn Anastasia <43697008+AnnaL001@users.noreply.github.com>
2023-11-28 23:26:38 +0300

    :test_tube: :construction: recycle(test): update test

--- app/src/androidTest/java/com/anna/healthyeats/HealthyEatsAuthTest.kt
+++ app/src/androidTest/java/com/anna/healthyeats/HealthyEatsAuthTest.kt
@@ -1,14 +1,34 @@
 package com.anna.healthyeats
 
+import com.anna.healthyeats.ui.screens.ui.auth.signup.state.SignUpScreenModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
 
 
+@HiltAndroidTest
+@RunWith(MockitoJUnitRunner.class)
+  class HealthyEatsAuthTest {
+  @get:Rule
+  var hiltRule = HiltAndroidRule(this)
 
-class HealthyEatsAuthTest {
+  @Mock
+  @Inject
+  lateinit var signUpScreenModel: SignUpScreenModel
+
+  @Before
+  open fun setup() {
+    hiltRule.inject()
+  }
+
   @Test
   fun testUserSignUpWithExistingEmailAndPassword_DisplaysErrorToast(){
     val email = "anna@gmail.com"
     val password = "testAnna01W$"
 
+    signUpScreenModel.signUpUser(email, password)
   }
 }
